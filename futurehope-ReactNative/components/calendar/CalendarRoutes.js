4b4a0c8e3d30bb7ad16e41e7be33c09d949e4d7b
tony <serratedhumor@yahoo.com>
2020-03-11 16:46:31 -0500

    device storage set up, add meeting functionality finished

--- futurehope-ReactNative/components/calendar/CalendarRoutes.js
+++ futurehope-ReactNative/components/calendar/CalendarRoutes.js
@@ -1,34 +1,55 @@
 
 import { View, Text, StyleSheet, FlatList, Alert, AsyncStorage, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { Route, useHistory } from "react-router-native"
 import CalendarView from './CalendarView'
 import AddEvent from './AddEvent'
-
+import uuid from 'react-native-uuid'
 const CalendarRoutes = () => {
 
-    const [agendaItems, setAgendaItems] = useState({
-        '2020-03-10': [{ text: 'Meeting with Bob Ross @ 3:15pm' }, { text: 'Meeting with Bob Ross @ 3:15pm' }, { text: 'Meeting with Bob Ross @ 3:15pm' }, { text: 'Meeting with Bob Ross @ 3:15pm' }, { text: 'Meeting with Bob Ross @ 3:15pm' }],
-        '2020-03-11': [{ text: "Meeting with Bob Ross @ 3:15pm" }],
-    })
-    const [date, setDate] = useState()
 
-    function submitHandler(date, mentor, time) {
-        const meetingText = `Meeting with ${mentor} @ ${time}`
-        setAgendaItems({
-            ...agendaItems,
-            [`${date}`]: [{ text: meetingText }]
+    const [agendaItems, setAgendaItems] = useState({})
+
+    React.useEffect(() => {
+        AsyncStorage.getItem("noteCalendarStorage").then(value => {
+            console.log(value)
+            if (value) {
+                setAgendaItems(JSON.parse(value))
+            } else {
+                setAgendaItems({})
+            }
         })
+    }, [])
+
+    React.useEffect(() => {
+        AsyncStorage.setItem("noteCalendarStorage", JSON.stringify(agendaItems))
+    }, [agendaItems])
+
+    function submitHandler(year, month, day, time, mentor) {
+        const date = `${year}-${month}-${day}`
+        const meetingText = `Meeting with ${mentor} @ ${time}`
+
+        agendaItems && agendaItems[`${date}`] ?
+            setAgendaItems({
+                ...agendaItems,
+                [`${date}`]: [...agendaItems[`${date}`], { id: uuid.v4(), text: meetingText }]
+            }) :
+            setAgendaItems({
+                ...agendaItems,
+                [`${date}`]: [{ id: uuid.v4(), text: meetingText }]
+            })
+
     }
 
     return (
 
         <View style={{ flex: 1 }}>
-            <Route exact path='/calendar/addEvent'><AddEvent date={date} submitHandler={submitHandler} /></Route>
-            <Route exact path='/calendar/agenda'><CalendarView agendaItems={agendaItems} setDate={setDate} /></Route>
+            <Route exact path='/calendar/addevent'><AddEvent submitHandler={submitHandler} /></Route>
+            <Route exact path='/calendar/agenda'><CalendarView agendaItems={agendaItems} /></Route>
         </View>
 
     )
 }
+
 
 export default CalendarRoutes
