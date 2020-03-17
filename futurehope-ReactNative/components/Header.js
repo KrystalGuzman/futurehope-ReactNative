6d271bd1d7a976a7d09bd4904bb862ad1e1434c7
jmjles <jmjles@gmail.com>
2020-03-17 10:28:58 -0700

    Styling changes [incomplete]

--- futurehope-ReactNative/components/Header.js
+++ futurehope-ReactNative/components/Header.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet, ShadowPropTypesIOS } from 'react-native';
-import { NativeRouter, Route, Link, useHistory } from "react-router-native"
-import Icon from 'react-native-vector-icons/FontAwesome'
+import React from "react";
+import { View, Text, StyleSheet, ShadowPropTypesIOS, StatusBar } from "react-native";
+import { NativeRouter, Route, Link, useHistory } from "react-router-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Header = () => {
-    const history = useHistory()
-    function backHandler() {
-        history.goBack()
-    }
+  const history = useHistory();
+  function backHandler() {
+    history.goBack();
+  }
 
-    return (
-        <View style={styles.header}>
-            <Icon style={styles.icon} name='arrow-left' onPress={backHandler} size={38} color='white' />
-            <Text style={styles.text}>School in the Sky</Text>
-            <Link style={styles.icon} to='/'><Icon name='home' color='white' size={38} /></Link>
-        </View>
-    )
-}
-
+  return (
+    <View style={styles.header}>
+        <StatusBar hidden/>
+      <Icon name="arrow-left" onPress={backHandler} size={24} color="black" />
+      <Text style={styles.text}>School in the Sky</Text>
+      <Link to="/">
+        <Icon name="home" color="black" size={24} />
+      </Link>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        height: 75,
-        paddingTop: 28,
-
-        backgroundColor: '#ff9800',
-        justifyContent: 'space-between'
-    },
-    text: {
-        color: 'white',
-        fontSize: 30,
-        textAlign: 'center'
-    },
-    icon: {
-        marginLeft: 20,
-        marginRight: 20
-    }
+  header: {
+    flexDirection: "row",
+    height: 75,
+    justifyContent: "space-around",
+    alignItems:'center'
+  },
+  text: {
+    color: "black",
+    fontSize: 16,
+    textAlign: "center"
+  }
 });
-
 
 export default Header;
--- futurehope-ReactNative/components/NoteTakingEdit.js
+++ futurehope-ReactNative/components/NoteTakingEdit.js
@@ -1,73 +1,127 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import { useParams } from 'react-router-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import { useParams } from "react-router-native";
 
 const EditItem = ({ items, editItem, edit, editChange }) => {
-    const { id } = useParams()
+  const { id } = useParams();
 
-    const [target, setTarget] = useState({})
+  const [target, setTarget] = useState({});
+  const [editNote, setEditNote] = useState(false);
+  React.useEffect(() => {
+    items.forEach(e => {
+      if (e.id === id) {
+        setTarget(e);
+      }
+    });
+  }, [items]);
 
-    React.useEffect(() => {
-        items.forEach(e => {
-
-            if (e.id === id) {
-                setTarget(e)
-            }
-        })
-    }, [items])
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{target.title}</Text>
-            <TextInput placeholder="Add details here..." style={styles.input} onChangeText={editChange} value={edit} />
-            <TouchableOpacity style={styles.button} onPress={() => editItem(edit, target.id)}>
-                <Text style={styles.buttonText}>Set Details</Text>
-            </TouchableOpacity>
-            <Text style={styles.title2}>Details</Text>
-            <Text style={styles.contents}>{target.content}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Text style={styles.title}>{target.title}</Text>
+        {!editNote ? (
+          <Text style={styles.edit} onPress={() => setEditNote(true)}>
+            Edit
+          </Text>
+        ) : null}
+      </View>
+      {!target.content ? (
+        <Text style={styles.placeHolder}>Add details here...</Text>
+      ) : (
+        <Text style={styles.contents}>{target.content}</Text>
+      )}
+{/*       <TextInput
+        placeholder="Add details here..."
+        style={styles.input}
+        onChangeText={editChange}
+        value={edit}
+      /> */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => editItem(edit, target.id)}
+      >
+        <Text style={styles.buttonText}>Set Details</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.editContentBtn}>
+          <Text style={styles.contentBtnText}>
+              Edit Content
+          </Text>
+      </TouchableOpacity>
+      </View>
+  );
 };
 
-
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    title2: {
-        fontSize: 30
-    },
-    title: {
-        fontSize: 40,
-        margin: 20
-    },
-    input: {
-        height: 60,
-        padding: 8,
-        fontSize: 16
-    },
-    button: {
-        backgroundColor: '#FFB23D',
-        padding: 9,
-        margin: 5,
-        width: 200,
-        borderRadius: 20
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 20,
-        textAlign: 'center'
-    },
-    contents: {
-        borderWidth: 1,
-        borderColor: 'black',
-        width: 400,
-        minHeight: 400,
-        padding: 7,
-        fontSize: 18
-    }
+  container: {
+    textAlign: "left",
+    marginLeft: 10,
+    marginRight: 10,
+    flex:1,
+  },
+  top: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  title2: {
+    fontSize: 30
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    margin: 20
+  },
+  edit: {
+    color: "#FFB23D",
+    textAlign: "right",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  placeHolder: {
+    color: "grey"
+  },
+  input: {
+    height: 60,
+    padding: 8,
+    fontSize: 16
+  },
+  button: {
+    backgroundColor: "#FFB23D",
+    padding: 9,
+    margin: 5,
+    width: 200,
+    borderRadius: 20
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    textAlign: "center"
+  },
+  contents: {
+    padding: 7,
+    color:'grey',
+    fontSize: 20
+  },
+  editContentBtn:{
+      width:'100%',
+      backgroundColor:'#FFB23D',
+      position:'absolute',
+      bottom:10
+  },
+  contentBtnText:{
+      color:'white',
+      textAlign:'center',
+      fontWeight:'bold',
+      padding:10,
+      fontSize:20
+  }
 });
-
 
 export default EditItem;
