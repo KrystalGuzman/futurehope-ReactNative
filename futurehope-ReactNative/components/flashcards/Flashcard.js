f8db65bd9309d7355f53230a06d271dc1745fe67
Hunter5555 <hunter.m.allan@gmail.com>
2020-05-14 16:06:18 -0600

    creates flashcard compoent and adds flashcard to footer. misc styling.

---
+++ futurehope-ReactNative/components/flashcards/Flashcard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Flashcard = () => {
+    return (
+        <View style={styles.flashcard}>
+            <Text>
+                Coming soon to a theater near you...
+            </Text>
+        </View>
+    )
+};
+
+export default Flashcard;
