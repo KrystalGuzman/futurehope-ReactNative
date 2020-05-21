2d8edcac99c9c7404a57b95832b1fc6c588c868e
Krystal Guzman <kryguzman@gmail.com>
2020-05-21 13:00:23 -0700

    works on syncing feature
    Co-authored-by: josiahbailey <josiahbailey101@yahoo.com>

--- futurehope-ReactNative/components/calendar/CalendarView.js
+++ futurehope-ReactNative/components/calendar/CalendarView.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, AsyncStorage } from 'react-native';
 import { useHistory } from 'react-router-native';
 import { Agenda } from 'react-native-calendars';
-import uuid from "react-native-uuid";
+import uuid from 'react-native-uuid';
 
 import { widthPercentageToDP, heightPercentageToDP } from '../../utils/PercenatageFix';
 
@@ -12,7 +12,7 @@ import AgendaItem from './AgendaItem';
 import moment from 'moment';
 import Firebase, { db } from '../../config/Firebase';
 
-const CalendarView = ({agendaItems2, setAgendaItems2}) => {
+const CalendarView = ({ agendaItems2, setAgendaItems2 }) => {
 	//{agendaItems, setAgendaItems}
 	const [ stateDate, setDate ] = useState(Date());
 	const [ events, setEvents ] = useState([]);
@@ -31,10 +31,11 @@ const CalendarView = ({agendaItems2, setAgendaItems2}) => {
 	});
 
 	const formatData = (arr) => {
-    let newData = [];
-    let date = ''
-    let meetingText = ''
-      
+		let newData = [];
+		let date = '';
+		let meetingText = '';
+		let newObj = {};
+
 		arr.map((doc) => {
 			const start = doc.start.seconds * 1000;
 			const start2 = new Date(start);
@@ -45,40 +46,57 @@ const CalendarView = ({agendaItems2, setAgendaItems2}) => {
 			const time = dt;
 			const mentor = doc.participantNames[0];
 			date = `${myDate[0]}-${myDate[1]}-${myDate[2]}`;
-      meetingText = `Meeting with ${doc.participantNames} at ${dt}`;
+			meetingText = `Meeting with ${doc.participantNames} at ${dt}`;
 
-      newData.push({
+			newData.push({
 				title: doc.title,
 				start: new Date(start),
 				id: doc.id,
 				mentor: doc.participantNames[0],
 				date: date,
-        meetingText: meetingText,
-        myDate: myDate,
+				meetingText: meetingText,
+				myDate: myDate,
 				time: time
-      });
+			});
 		});
 
-    const newArr = [];
-    let newObj = {};
+		const checkItem = (obj, item) => {
+			if (obj && obj[item.date]) {
+				obj[item.date].push({ id: item.id, text: item.meetingText ? item.meetingText : item.text });
+			} else {
+				obj[item.date] = [ { id: item.id, text: item.meetingText ? item.meetingText : item.text } ];
+			}
+		}
 
-    newData.map((item) => {
-      // newArr.push({ [item.date]: [ { id: item.id, text: item.meetingText } ] });
-      // newObj[date] ? newObj[date].push({ id: item.id, text: item.meetingText }) : newObj[date] = [{ id: item.id, text: item.meetingText }
-      if (newObj && newObj[item.date]) { 
-        // if (item.date === item.date) {
-          newObj[item.date].push({ id: item.id, text: item.meetingText })
-        // }
-      } else {
-        newObj[item.date] = [{ id: item.id, text: item.meetingText }]
-      }
-    });
-    
+		let localEvents = {}
+		AsyncStorage.getItem("noteCalendarStorage").then(value => {
+		// const data = Object.entries(value)
+		// console.log(value)
+		// Object.entries(value).map((item) => { 
+		// 	// console.log(item)
+		// 	// item[1] ? item[1].map(subItem => { 
+		// 	// 	console.log(subItem)
+		// 	// }) : ''
+		// })
+
+			// console.log(value)
+			localEvents = value
+			console.log(value)
+			// Object.entries(localEvents).map((item) => { 
+			// 	console.log(item)
+			// });
+
+		});
+
+		newData.map((item) => {
+			checkItem(newObj, item)
+		});
+
 		setAgendaItems(newObj);
 		setEvents(newData);
 	};
 
-	const getStorageValue = async () => {
+	const getStorageValue = async (e) => {
 		const uid = await AsyncStorage.getItem('UID');
 		db.collection('meetings').where('participantUIDs', 'array-contains', uid).onSnapshot((querySnapshot) => {
 			const arr = [];
@@ -92,13 +110,16 @@ const CalendarView = ({agendaItems2, setAgendaItems2}) => {
 	};
 
 	useEffect(() => {
-    getStorageValue();
-  }, []);
-  // console.log('STATE DATE', stateDate)
-  console.log('ITEMS', agendaItems);
+		getStorageValue();
+	}, []);
+	// console.log('STATE DATE', stateDate)
+	// console.log('ITEMS', agendaItems);
 
-  // { [date]: agendaItems[date] ? [ ...agendaItems[date] ] : [] }
+	// { [date]: agendaItems[date] ? [ ...agendaItems[date] ] : [] }
+//delete point
 
+  // { [stateDate]: agendaItems[stateDate] ? [ ...agendaItems[stateDate] ] : [] }
+  // console.log("AI", agendaItems);
 	return (
 		<View style={{ flex: 1 }}>
 			<Agenda
@@ -108,10 +129,10 @@ const CalendarView = ({agendaItems2, setAgendaItems2}) => {
 				renderItem={(item) => {
 					return (
 						<AgendaItem
-						item={item}
-            setAgendaItems={setAgendaItems}
-            agendaItems={agendaItems}
-            date={stateDate}
+							item={item}
+							setAgendaItems={setAgendaItems}
+							agendaItems={agendaItems}
+							date={stateDate}
 						/>
 					);
 				}}
@@ -120,58 +141,61 @@ const CalendarView = ({agendaItems2, setAgendaItems2}) => {
 				<Text style={styles.button} onPress={onPress}>
 					Add an Event
 				</Text>
+				<Text style={styles.button} onPress={getStorageValue}>
+					Sync
+				</Text>
 			</View>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-  text: {
-    padding: 15,
-    backgroundColor: "#f3f3f3",
-    borderBottomWidth: 1,
-    borderColor: "#eee",
-    fontSize: 25
-  },
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: heightPercentageToDP(".5%"),
-    backgroundColor: "#f3f3f3"
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    margin: 20,
-  },
-  modal: {
-    width: 200,
-  },
-  button: {
-    textAlign: "center",
-    fontSize: 26.5,
-    paddingLeft: widthPercentageToDP("15%"),
-    paddingRight: widthPercentageToDP("15%"),
-    paddingTop: widthPercentageToDP("3%"),
-    paddingBottom: widthPercentageToDP("3%"),
-    backgroundColor: "#FFA611",
-    color: "white",
-    marginBottom: 20
-  },
-  input: {
-    margin: 20
-  },
-  itemView: {
-    height: 100,
-    borderBottomColor: "black",
-    borderBottomWidth: 2,
-    justifyContent: "space-around",
-    marginRight: 20,
-    marginBottom: 5
-  },
-  itemText: {
-    fontSize: 18
-  }
+	text: {
+		padding: 15,
+		backgroundColor: '#f3f3f3',
+		borderBottomWidth: 1,
+		borderColor: '#eee',
+		fontSize: 25
+	},
+	container: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingBottom: heightPercentageToDP('.5%'),
+		backgroundColor: '#f3f3f3'
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		margin: 20
+	},
+	modal: {
+		width: 200
+	},
+	button: {
+		textAlign: 'center',
+		fontSize: 26.5,
+		paddingLeft: widthPercentageToDP('15%'),
+		paddingRight: widthPercentageToDP('15%'),
+		paddingTop: widthPercentageToDP('3%'),
+		paddingBottom: widthPercentageToDP('3%'),
+		backgroundColor: '#FFA611',
+		color: 'white',
+		marginBottom: 20
+	},
+	input: {
+		margin: 20
+	},
+	itemView: {
+		height: 100,
+		borderBottomColor: 'black',
+		borderBottomWidth: 2,
+		justifyContent: 'space-around',
+		marginRight: 20,
+		marginBottom: 5
+	},
+	itemText: {
+		fontSize: 18
+	}
 });
 
 export default CalendarView;
